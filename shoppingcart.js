c03fcfb34038f05cef769bca8080bb0b8a474888
majadey4u <maryjaneogbuju@gmail.com>
2024-03-02 14:47:15 +0100

    comments

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,9 +1,13 @@
+
+  // Function to increase quantity
 function increaseQuantity(button) {
   var input = button.parentNode.querySelector('input');
   input.value = parseInt(input.value) + 1;
   updateTotalPrice();
 }
 
+
+  // Function to decrease quantity
 function decreaseQuantity(button) {
   var input = button.parentNode.querySelector('input');
   if (parseInt(input.value) > 1) {
@@ -11,16 +15,19 @@ function decreaseQuantity(button) {
     updateTotalPrice();
   }
 }
-
+  // Function to remove item from cart
 function removeItem(button) {
   button.closest('.cart-item').remove();
   updateTotalPrice();
 }
 
+
+ // Function to toggle like status
 function toggleLike(icon) {
   icon.classList.toggle('active');
 }
 
+ // Function to update total price
 function updateTotalPrice() {
   var total = 0;
   var items = document.querySelectorAll('.cart-item');
